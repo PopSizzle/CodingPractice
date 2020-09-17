a3bb594d013fe2f1ca84436d48fbff1a866b513a
Sam Poppe <poppe.samuel@gmail.com>
2020-09-17 15:44:43 -0700

    adding counting negatives problem

---
+++ LeetCode Problems/negativeCount.js
@@ -0,0 +1,18 @@
+// Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise. 
+
+// Return the number of negative numbers in grid.
+
+var countNegatives = function(grid) {
+    let count = 0;
+    
+    for(let i=0; i<grid.length; i++){
+        for(let j=0; j<grid[i].length; j++){
+            if(grid[i][j] < 0){
+                count += grid[i].length - j;
+                break;
+            }
+        }
+    }
+    
+    return count;
+};
