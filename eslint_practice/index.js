476041ff3a580ccef82a6a0bdb3ff1baa4c41678
Sam Poppe <poppe.samuel@gmail.com>
2020-09-30 19:19:36 -0700

    adding apiRoutes

--- eslint_practice/index.js
+++ eslint_practice/index.js
@@ -3,6 +3,5 @@ const express = require('express');
 const app = express;
 
 app.listen(3001, () => {
-    console.log('Server started listening on PORT http://localhost:3001');
+  console.log('Server started listening on PORT http://localhost:3001');
 });
-
