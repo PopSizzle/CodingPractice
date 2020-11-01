72e58ba8c5fb07ab9f63e3a571d84364eeb21070
Sam Poppe <poppe.samuel@gmail.com>
2020-11-01 11:43:51 -0800

    max width problem

---
+++ LeetCode Problems/maxWidth.js
@@ -0,0 +1,26 @@
+// Given n points on a 2D plane where points[i] = [xi, yi], Return the widest vertical area between two points such that no points are inside the area.
+
+// A vertical area is an area of fixed-width extending infinitely along the y-axis (i.e., infinite height). The widest vertical area is the one with the maximum width.
+
+// Note that points on the edge of a vertical area are not considered included in the area.
+
+var maxWidthOfVerticalArea = function(points) {
+   
+  points.sort(function(a, b){
+      return a[0] - b[0]
+  })
+  
+  console.log(points);
+  
+  let greatest = 0;
+  
+  for(let i=1; i<points.length; i++){
+      let width = points[i][0] - points[i-1][0];
+      
+      if(width > greatest){
+          greatest = width;
+      }
+  }
+  
+  return greatest;
+};
